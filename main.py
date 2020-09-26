76ac8ff7b615336be2ec9e1a36b9f595950fc337
Marcelo Terra <62788203+MarceloTerra0@users.noreply.github.com>
2020-09-26 02:49:42 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+#requirements.txt
+from gtts import gTTS
+from pygame import mixer
+from mutagen.mp3 import MP3
+
+#native python
+import time
+import os
+from math import ceil
+
+#this is used for cleaning the temp file that we create
+flag = 0
+file_names = ["one.mp3", "two.mp3"]
+
+while True:
+
+    if flag == 0:
+        flag = 1
+    else:
+        flag = 0
+
+    #apparently this only works in pygame==2.0.0.dev8, 
+    #since my 1.9.6 version gave me a lot of headaches
+    mixer.init(devicename='CABLE Input (VB-Audio Virtual Cable)')
+    usr_text = input("Type the text to be spoken: ")
+
+    if not usr_text:
+        continue
+
+    #tts = gTTS(text = usr_text, lang ="pt") = Brazilian portuguese
+    #if lang is not provided, US English is the default one
+    tts = gTTS(text = usr_text)
+    #sadly we need to save this to a file, instead of using it in memory
+    #(at least I don't know how)
+    tts.save(file_names[flag])
+
+    mixer.music.load(file_names[flag])
+    mixer.music.play()
+    audio = MP3(file_names[flag])
+    time.sleep(ceil(audio.info.length) + 0.3)
+    mixer.stop()
+
+    #this is needed so that there are no permission problems
+    #since pygame is really finnicky with the mp3 files it opens
+    if os.path.exists(os.path.abspath(file_names[abs(1-flag)])):
+        os.remove(os.path.abspath(file_names[abs(1-flag)]))
